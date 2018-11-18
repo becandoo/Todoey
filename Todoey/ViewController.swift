5f091e086b3fe1f3a3501729dfc1a3f2e3335803
Brandon Cranmore <bec@brandons-macbook-pro.local>
2018-11-18 11:52:29 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Brandon Cranmore on 11/18/18.
+//  Copyright © 2018 Brandon Cranmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
